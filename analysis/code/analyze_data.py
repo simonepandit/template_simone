b8ad1151618144f8f742e47073277bb53a2344bb
Simone Pandit <simone.pandit@gmail.com>
2023-07-21 09:08:33 -0700

    #3 Update analyze_data.py for additional regression

--- analysis/code/analyze_data.py
+++ analysis/code/analyze_data.py
@@ -6,11 +6,18 @@ from linearmodels import PanelOLS
 def main():
     df = import_data()
     fit = run_regression(df)
+    df_1960 = df[df['year']>=1960]
+    fit1960 = run_regression(df_1960)
     formatted = format_model(fit)
+    formatted_1960 = format_model(fit1960)
     
     with open('output/regression.csv', 'w') as f:
         f.write('<tab:regression>' + '\n')
         formatted.to_csv(f, sep = '\t', index = False, header = False)
+     
+    with open('output/regression_1960.csv', 'w') as f:
+        f.write('<tab:regression_1960>' + '\n')
+        formatted_1960.to_csv(f, sep = '\t', index = False, header = False)
     
 def import_data():
     df = pd.read_csv('input/data_cleaned.csv')
@@ -31,7 +38,8 @@ def format_model(fit):
                               'p_value'  : fit.pvalues})
     formatted = formatted.loc[['post_tv']]
     
-    return(formatted)
+    return(formatted)  
+       
     
 ### EXECUTE
 main()
